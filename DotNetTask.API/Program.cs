92aa0335ef88898548417ba467be7f7c7db35882
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 17:29:56 +0100

    program service and repositories added to the DI container in the program.cs for dependencies resolution during runtime

--- DotNetTask.API/Program.cs
+++ DotNetTask.API/Program.cs
@@ -1,4 +1,12 @@
+using DotNetTask.API.CosmosSetUp;
+using DotNetTask.API.Services.Implementations;
+using DotNetTask.API.Services.Interfaces;
+using DotNetTask.Data.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var _configuration = builder.Configuration.GetSection("CosmosDb");
+var _initializeCosmos = new CosmosInitializer(_configuration);
 
 // Add services to the container.
 
@@ -6,6 +14,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProgramService, ProgramService>();
+builder.Services.AddSingleton<IProgramRepository>(_initializeCosmos.InitializeProgramRepositoryInstanceAsync().GetAwaiter().GetResult());
+builder.Services.AddSingleton<IApplicationRepository>(_initializeCosmos.InitializeApplicationRepositoryInstanceAsync().GetAwaiter().GetResult());
 
 var app = builder.Build();
 
