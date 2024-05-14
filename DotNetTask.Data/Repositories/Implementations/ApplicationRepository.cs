8e97b92b1e01605842803398b2b7a11df828b39f
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 16:21:46 +0100

    Microsoft.azure.cosmos package added to data layer for interacting with cosmos db using Linq and application repository implemented

---
+++ DotNetTask.Data/Repositories/Implementations/ApplicationRepository.cs
@@ -0,0 +1,32 @@
+﻿using DotNetTask.Data.Entities;
+using DotNetTask.Data.Repositories.Interfaces;
+using Microsoft.Azure.Cosmos;
+
+namespace DotNetTask.Data.Repositories.Implementations
+{
+    public class ApplicationRepository : IApplicationRepository
+    {
+        private readonly Container _container;
+
+        public ApplicationRepository(CosmosClient cosmosDbClient, string databaseName, string containerName)
+        {
+            _container = cosmosDbClient.GetContainer(databaseName, containerName);
+        }
+        public async Task<Application> AddApplicationAsync(Application application)
+        {
+            var response = await _container.CreateItemAsync(application, new PartitionKey(application.Id));
+            return response.Resource;
+        }
+
+        public async Task<Application> GetApplicationWithEmailAsync(string email)
+        {
+            var query = new QueryDefinition("SELECT * FROM Applications a WHERE a.Email = @Email")
+                                            .WithParameter("@Email", email);
+
+            var iterator = _container.GetItemQueryIterator<Application>(query);
+            var response = await iterator.ReadNextAsync();
+
+            return response.FirstOrDefault();
+        }
+    }
+}
