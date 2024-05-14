1c2538c3c49b3c39c0e5a0ed33a682d68f735dea
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 17:18:35 +0100

    program controller implemented

---
+++ DotNetTask.API/Controllers/ProgramController.cs
@@ -0,0 +1,46 @@
+﻿using DotNetTask.API.Services.Interfaces;
+using DotNetTask.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotNetTask.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgramController : ControllerBase
+    {
+        private readonly IProgramService _programService;
+        public ProgramController(IProgramService programService)
+        {
+            _programService = programService ?? throw new ArgumentNullException(nameof(programService));
+        }
+
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProgramRequest request)
+        {
+            var response = await _programService.AddProgramAsync(request);
+            return Ok(response);
+        }
+
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(string id, [FromBody] UpdateProgramRequest request)
+        {
+            var response = await _programService.EditProgramAsync(id, request);
+            return Ok(response);
+        }
+
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProgramQuestions(string id)
+        {
+            var response = await _programService.GetProgramQuestionsByIdAsync(id);
+            if (response == null)
+                return NotFound();
+
+            return Ok(response);
+        }
+    }
+}
