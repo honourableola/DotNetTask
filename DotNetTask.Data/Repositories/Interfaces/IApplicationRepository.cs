4a7df464b6fd0082005b7961aa413595b50acb80
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 16:13:16 +0100

    Interfaces for user Application and Program Repositories defined

---
+++ DotNetTask.Data/Repositories/Interfaces/IApplicationRepository.cs
@@ -0,0 +1,10 @@
+﻿using DotNetTask.Data.Entities;
+
+namespace DotNetTask.Data.Repositories.Interfaces
+{
+    public interface IApplicationRepository
+    {
+        Task<Application> AddApplicationAsync(Application application);
+        Task<Application> GetApplicationWithEmailAsync(string email);
+    }
+}
---
+++ DotNetTask.Data/Repositories/Interfaces/IProgramRepository.cs
@@ -0,0 +1,11 @@
+﻿using DotNetTask.Data.Entities;
+
+namespace DotNetTask.Data.Repositories.Interfaces
+{
+    public interface IProgramRepository
+    {
+        Task<ProgramData> AddProgramAsync(ProgramData program);
+        Task<ProgramData> UpdateProgramAsync(string id, ProgramData program);
+        Task<ProgramData> GetProgramQuestionsByIdAsync(string id);
+    }
+}
