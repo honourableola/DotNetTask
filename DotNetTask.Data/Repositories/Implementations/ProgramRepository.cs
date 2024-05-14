bf6f7c963958520a219a14f9587f90ed0f04e53a
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 16:33:04 +0100

    program repository implemented

---
+++ DotNetTask.Data/Repositories/Implementations/ProgramRepository.cs
@@ -0,0 +1,33 @@
+﻿using DotNetTask.Data.Entities;
+using DotNetTask.Data.Repositories.Interfaces;
+using Microsoft.Azure.Cosmos;
+
+namespace DotNetTask.Data.Repositories.Implementations
+{
+    public class ProgramRepository : IProgramRepository
+    {
+        private readonly Container _container;
+
+        public ProgramRepository(CosmosClient cosmosDbClient, string databaseName, string containerName)
+        {
+            _container = cosmosDbClient.GetContainer(databaseName, containerName);
+        }
+        public async Task<ProgramData> AddProgramAsync(ProgramData program)
+        {
+            var response = await _container.CreateItemAsync(program, new PartitionKey(program.Id));
+            return response.Resource;
+        }
+
+        public async Task<ProgramData> GetProgramQuestionsByIdAsync(string id)
+        {
+            var response = await _container.ReadItemAsync<ProgramData>(id, new PartitionKey(id));
+            return response.Resource;
+        }
+
+        public async Task<ProgramData> UpdateProgramAsync(string id, ProgramData program)
+        {
+            var response = await _container.UpsertItemAsync(program, new PartitionKey(id));
+            return response.Resource;
+        }
+    }
+}
