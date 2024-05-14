37c6f44a5fadf343b522f37c8332546ee9abac6e
Yunus Olalekan <olalekanyunus2013@gmail.com>
2024-05-14 17:51:38 +0100

    application controller implemented

---
+++ DotNetTask.API/Controllers/ApplicationController.cs
@@ -0,0 +1,26 @@
+﻿using DotNetTask.API.Services.Interfaces;
+using DotNetTask.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DotNetTask.API.Controllers
+{
+    [Route("api/applications")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly IApplicationService _applicationService;
+        public ApplicationController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService ?? throw new ArgumentNullException(nameof(applicationService));
+        }
+
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        [HttpPost]
+        public async Task<IActionResult> Create(string programId, [FromBody] ApplicationRequest item)
+        {
+            var task = await _applicationService.AddApplicationAsync(programId, item);
+            return Ok(task);
+        }
+    }
+}
